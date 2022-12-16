116bdd8499d589f9e4b62c816b49ed8b6668186c
Benjamín Delgado <101227603+Benjamin-mne@users.noreply.github.com>
2022-12-16 20:18:06 -0300

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -15,10 +15,10 @@ export const About = () => {
       <h2>About Me</h2>
       <div className='container about__container'>
         <div className='about__me' >
-          <img src={Me} alt="about-img" className='hobbyes about__me-image'/>
+          <img src={Me} alt="about-img" className='Hobbies about__me-image'/>
           <div className='about__me-img-icon' >
               {current === 'studies' && <ImBooks className='about__icon' style={{fontSize:'10rem'}}/>}
-              {current === 'hobbies' && <MdPiano className='about__icon' style={{fontSize:'10rem'}}/>}
+              {current === 'Hobbies' && <MdPiano className='about__icon' style={{fontSize:'10rem'}}/>}
               {current === 'goals' && <GiStairsGoal className='about__icon' style={{fontSize:'10rem'}}/>}
           </div>
         </div>
@@ -30,9 +30,9 @@ export const About = () => {
               <small>1  Year Math Teacher</small><br/>
               <small>2 Years Engineering</small>
             </article>
-            <article className='about__card' onMouseEnter={() => setCurrent('hobbyes')}>
+            <article className='about__card' onMouseEnter={() => setCurrent('Hobbies')}>
               <MdPiano className='about__icon' />
-              <h5>Hobbyes</h5>
+              <h5>Hobbies</h5>
               <small>Musical Production</small><br/>
               <small>Play Chess</small>
             </article>
@@ -46,7 +46,7 @@ export const About = () => {
           { current === 'studies' && 
           <p style={{minHeight:'76px'}}>I studied <a>Information Systems Engineering</a> for more than two years at the National Technological University, where I found a love for mathematics and programming. In 2022 I returned to my hometown to start the <a>Mathematics Teaching</a> career that I am currently studying.</p>
           }
-          { current === 'hobbyes' && 
+          { current === 'Hobbies' && 
           <p style={{minHeight:'76px'}}>In my free time I like to <a>listen and produce music</a> <small><a href='https://open.spotify.com/artist/0SyapgohHwceqcP7mJpAkq?si=JP0g923JTYi5dqgQlHLm6g' target='_blank' >(listen on Spotify)</a></small> I also enjoy playing <a>video games</a> or <a>board games</a> with friends, reading a <a>book</a>, watching a <a>movie, video</a> or <a>serie</a>.</p>
           }
           { current === 'goals' && 
